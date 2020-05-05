f65e6079e3effdaf66fc605d5067d561180d9e11
arajo-hub <joara9566@naver.com>
2020-05-05 13:32:15 +0900

    byara

--- SNSclone/posts/urls.py
+++ SNSclone/posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='posts'
 
 urlpatterns=[
-    path('/', views.PostList.as_view(), name='all'),
+    path('', views.PostList.as_view(), name='all'),
     path('new/', views.CreatePost.as_view(), name='create'),
     path('by/<username>/', views.UserPosts.as_view(), name='for_user'),
     path('by/<username>/<int:pk>/', views.PostDetail.as_view(), name='single'),
