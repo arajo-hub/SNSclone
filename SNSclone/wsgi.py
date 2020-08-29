f7a4ec1b4778d0b8417393212c7b56d76602cca7
Yoodahun <dahun4032@gmail.com>
2020-08-29 22:56:48 +0900

    add application

--- SNSclone/wsgi.py
+++ SNSclone/wsgi.py
@@ -10,5 +10,7 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from SNSclone.wsgi import SNSclone
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SNSclone.settings')
+application = SNSclone(application)
